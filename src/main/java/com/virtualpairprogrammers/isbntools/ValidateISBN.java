bf632e7ffa259f2caf1ef9ec77e7293065d1bcc1
Robert Fowler <robertf@ipay.co.za>
2018-09-11 15:14:08 +0200

    Setup project for TDD course.

---
+++ src/main/java/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -0,0 +1,7 @@
+package com.virtualpairprogrammers.isbntools;
+
+/**
+ * Created by robertf on 2018/09/11
+ */
+public class ValidateISBN {
+}
---
+++ src/test/java/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -0,0 +1,17 @@
+package com.virtualpairprogrammers.isbntools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by robertf on 2018/09/11
+ */
+public class ValidateISBNTest {
+
+    @Test
+    public void test() {
+//        fail("Not yet implemented!");
+    }
+
+}
