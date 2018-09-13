05d6eb2dd10d58e777ef449c0fcb689732867648
Robert Fowler <robertf@ipay.co.za>
2018-09-13 20:04:16 +0200

    Convert basic java project ot gradle. Add unit test stuff.

--- src/test/java/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ src/test/java/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -2,16 +2,61 @@ package com.virtualpairprogrammers.isbntools;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-/**
- * Created by robertf on 2018/09/11
- */
 public class ValidateISBNTest {
 
+
     @Test
-    public void test() {
-//        fail("Not yet implemented!");
+    public void checkAValid10DigitISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue("first value", result);
+        result = validator.checkISBN("0140177396");
+        assertTrue("second value", result);
+    }
+
+    @Test
+    public void checkAValid13DigitISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9781853260087");
+        assertTrue("first value", result);
+        result = validator.checkISBN("9781853267338");
+        assertTrue("second value", result);
+    }
+
+    @Test
+    public void TenDigitISBNNumbersEndingInAnXAreValid() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("012000030X");
+        assertTrue(result);
+    }
+
+    @Test
+    public void checkAnInvalid10DigitISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449117");
+        assertFalse(result);
+    }
+
+    @Test
+    public void checkAnInvalid13DigitISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9781853267336");
+        assertFalse(result);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void nineDigitISBNsAreNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        validator.checkISBN("123456789");
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void nonNumericISBNsAreNotAllowed() {
+        ValidateISBN validator = new ValidateISBN();
+        validator.checkISBN("helloworld");
     }
 
 }
